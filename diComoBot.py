6879e4ab58f5d38ff7dd79040912efb02a662a71
Jordan Simkovic <jks3@illinois.edu>
2018-06-11 02:27:50 -0400

    Added files for diComo, Puma, and Chams

---
+++ diComoBot.py
@@ -0,0 +1,57 @@
+import praw
+import tweepy
+import json
+from tweepy import Stream
+class MyStreamListener(tweepy.StreamListener):
+    auth1 = tweepy.OAuthHandler("BZ1mLujonEsiQ1nXHsQRL5qQQ",
+                               "PeuWzllsoiXnQBxAgWLInNUM8BBY2I0eXQZ2yB2pIp59Fjt4Ul")
+    auth1.set_access_token("615513817-1IxaVyxPfkxZ6jIO9CI89b4FJpSUYxUfw42iUZEH",
+                          "aL5qn9629Q3kvCkWjRlKA6bYXwyTnKxaskTLMwsZvmBlI")
+
+    #api1 = tweepy.API(auth1)
+    def on_status(self, status, api1=tweepy.API(auth1)):
+        self.stream = Stream(auth=api1.auth, listener=self, tweet_mode='extended')
+
+        try:
+            print(status.extended_tweet['full_text'])
+
+            reddit.subreddit("nba").submit(
+                title= "[Adrian Wojnarowski] "
+                       + status.extended_tweet['full_text']
+                       [0:status.extended_tweet['full_text'].find(".") + 1],
+                url="https://twitter.com/JordanSimkovic/status/"
+                                                   + str(status.id))
+        except:
+            print(status.text)
+
+            reddit.subreddit("nba").submit(
+                title="[Adrian Wojnarowski] "
+                      + status.text[0:status.text.find(".") + 1]
+                , url="https://twitter.com/JordanSimkovic/status/"
+                                                  + str(status.id))
+
+
+
+
+reddit = praw.Reddit(client_id='jvTpt-_A6Y_oTA',
+                     client_secret='lMkkD-4s2fPkxE9Kp--VrCEHoMI',
+                     user_agent='Woj bot by u/mkgandkembafan',
+                     username='mkgandkembafan',
+                     password='Jkys1171998!?')
+
+auth = tweepy.OAuthHandler("BZ1mLujonEsiQ1nXHsQRL5qQQ",
+                           "PeuWzllsoiXnQBxAgWLInNUM8BBY2I0eXQZ2yB2pIp59Fjt4Ul")
+auth.set_access_token("615513817-1IxaVyxPfkxZ6jIO9CI89b4FJpSUYxUfw42iUZEH",
+                      "aL5qn9629Q3kvCkWjRlKA6bYXwyTnKxaskTLMwsZvmBlI")
+
+api = tweepy.API(auth)
+
+print(api.user_timeline(id = "wojespn", tweet_mode = "extended")[2].full_text)
+
+print(type(api.user_timeline(id = "wojespn", tweet_mode = "extended")[2]))
+
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener, tweet_mode = "extended")
+
+myStream.filter(follow=["56855530"])
+
